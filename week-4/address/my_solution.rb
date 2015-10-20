532a17103aa0970979aa551c41fb6358c01f9bdb
nowakoff <anowak@icstars.org>
2015-10-21 00:24:49 +0200

    mini chalenges

---
+++ week-4/address/my_solution.rb
@@ -0,0 +1,6 @@
+def make_address(street, city , state,  zip )
+p "You live at #{street}, in the beautiful city of #{city}, #{state}. Your zip is #{zip}."
+  
+end
+
+make_address("633 Folsom St.", "San Francisco", "CA", "94107")
---
+++ week-4/math/my_solution.rb
@@ -0,0 +1,26 @@
+# Math Methods
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def add(num_1, num_2)
+ #your code here
+ num_1 + num_2
+end
+
+def subtract(num_1, num_2)
+ #your code here
+ num_1 -num_2
+end
+
+def multiply(num_1, num_2)
+ #your code here
+ num_1 *num_2
+end
+
+def divide(num_1, num_2)
+ #your code here
+ num_1.to_f / num_2
+end
