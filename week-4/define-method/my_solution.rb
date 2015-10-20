338730de6d1dd94edced8bf180b60936b6cc2d06
nowakoff <anowak@icstars.org>
2015-10-21 00:12:22 +0200

    small ruby chalenges

---
+++ week-4/define-method/my_solution.rb
@@ -0,0 +1,12 @@
+# Define an Empty Method
+def do_something(x,y,z)
+end
+
+
+
+
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
---
+++ week-4/variables-methods.rb
@@ -0,0 +1,17 @@
+```ruby
+puts "What is your name?"
+name = gets.chomp
+puts "What is your middle name?"
+middle = gets.chomp
+puts "What is your last name?"
+last = gets.chomp
+"Hey! Nice yo see you #{name} #{middle} #{last}"
+```
+
+
+```ruby
+puts "What your favourite number?"
+number = gets.chomp 
+number += 1
+puts "Isn't #{number} is better and bigger?"
+```
