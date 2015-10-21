12666741d1c8f1d779b9ec08cd72a20e98c6e3c8
nowakoff <anowak@icstars.org>
2015-10-21 18:28:38 +0200

    good guess

---
+++ week-4/good-guess/my_solution.rb
@@ -0,0 +1,15 @@
+# Good Guess
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+ def good_guess? number
+  if number == 42
+    true
+  else 
+    false
+  end
+end
+
+p good_guess?  42
