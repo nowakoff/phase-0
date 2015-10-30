3e3221f52525ce8ebced9cd1322d58ba6ae45261
nowakoff <anowak@icstars.org>
2015-10-30 01:42:12 +0100

    pad-array

--- week-5/pad-array/my_solution.rb
+++ week-5/pad-array/my_solution.rb
@@ -13,23 +13,70 @@
 
 # 0. Pseudocode
 
-# What is the input?
+# What is the input? 
+#  array, integer value, nil/any object 
 # What is the output? (i.e. What should the code return?)
+#  array
 # What are the steps needed to solve the problem?
+   # compare length of array with min_size arugument 
+   # subtract length of array from min_value
+   # if the min_value is grater from the length of array we add the third arugument to an array that many times 
+
+
 
 
 # 1. Initial Solution
-def pad!(array, min_size, value = nil) #destructive
-  # Your code here
-end
+# def pad!(array, min_size, value = nil) #destructive
+#   # Your code here
+#   	if min_size <= array.size
+# 		return array 
+# 	else
+# 	  	for i in 1..min_size - array.size 
+# 	  		array.push(value)
+# 		end
+# 	return array
+# 	end
+# end
 
-def pad(array, min_size, value = nil) #non-destructive
-  # Your code here
-end
+# def pad(array, min_size, value = nil) #non-destructive
+#   # Your code here
+#   	my_array = [] 
+#   	array.each { |x| my_array.push(x) }
+# 	if min_size <= array.size
+# 		return my_array 
+# 	else
+# 	  	for i in 1..min_size - array.size 
+# 	  		my_array.push(value)
+# 		end
+# 	return my_array
+# 	end
+# end
 
 
 # 3. Refactored Solution
 
+def pad!(array, min_size, value = nil) #destructive
+  # Your code here
+	if min_size > array.size
+	  	(min_size - array.size).times { |x| array.push(value) }
+	end
+	return array
+end
+
+def pad(array, min_size, value = nil) #non-destructive
+  # Your code here
+  	my_array = [] 
+  	array.each { |x| my_array.push(x) }
+	if min_size > array.size
+	  	(min_size - array.size).times { |x| my_array.push(value) }
+	end
+	return my_array
+end
 
 
 # 4. Reflection
+
+
+
+
+
