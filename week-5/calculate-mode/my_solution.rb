ec90e6301016b0b30d21537c6ab0a9e865a47a7c
nowakoff <anowak@icstars.org>
2015-10-31 18:57:43 +0100

    reflection

--- week-5/calculate-mode/my_solution.rb
+++ week-5/calculate-mode/my_solution.rb
@@ -60,4 +60,23 @@ end
 
 mode( [1,2,3,4,5,5,6,6,7] )
 
+
 # 4. Reflection
+
+
+
+Which data structure did you and your pair decide to implement and why?
+We used hash and array. Hash ebcouse we needed to pair some values and array becouse it is easier to use. 
+
+Were you more successful breaking this problem down into implementable pseudocode than the last with a pair? 
+I would say it was harder. With last solution we hade answer in our head right away. Here we had to use 100 of different methods and approches to make solution work and pseudocode was not enough specific.
+
+What issues/successes did you run into when translating your pseudocode to code?
+Our pseudocde was not enough specific. Also, we could not decrease steps in our code. 
+
+What methods did you use to iterate through the content? Did you find any good ones when you were refactoring? Were they difficult to implement?
+We used each and max and max_by. We find a max method which helped us to decrease the code a little bit. They where easy yo implement. 
+
+
+
+
