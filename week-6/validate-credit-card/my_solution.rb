317298415d8ac195f2e344d2422a017b29385dc9
nowakoff <anowak@icstars.org>
2015-11-05 02:09:23 +0100

    validate card

--- week-6/validate-credit-card/my_solution.rb
+++ week-6/validate-credit-card/my_solution.rb
@@ -6,8 +6,8 @@
 
 # Pseudocode
 
-# Input:
-# Output:
+# Input: integer
+# Output: true/false
 # Steps:
 
 
@@ -18,7 +18,58 @@
 
 class CreditCard
 
+	def initialize(number)
+		@number = number.to_s.chars.map(&:to_i)
+		raise ArgumentError.new("Your number is not a valid") unless @number.size == 16
+	end
+
+	def double 
+		new_array = []
+		@number_reverse = @number.reverse
+		for x in 1...@number_reverse.size
+			if x%2 != 0
+			multiply_num = @number_reverse[x] * 2
+			new_array << multiply_num
+		else 
+			new_array << @number_reverse[x]
+		end
+		
+		end
+		p new_array.reverse << @number[-1]
+		@array = new_array.reverse << @number[-1]
+		end
+
+		def sum
+			new = []
+			@array.join.chars.each do |x|
+				new << x.to_i
+			end
+			@final =  new.inject(:+)
+		end
+
+		def check_card
+
+			if @final % 10 == 0 
+				p true
+			else
+				p false
+			end
+
+		end
+
 end
+
+
+card = CreditCard.new(4408041234567901)
+card.check_card
+num = CreditCard.new(1111989846351897)
+num.double
+num.sum
+num.check_card
+
+number = 1111222233334444
+numberM = number.to_s.chars.map(&:to_i)
+p numberM.size
 
 
 
@@ -32,3 +83,7 @@ end
 
 
 # Reflection
+
+# What was the most difficult part of this challenge for you and your pair?
+# What new methods did you find to help you when you refactored?
+# What concepts or learnings were you able to solidify in this challenge?
