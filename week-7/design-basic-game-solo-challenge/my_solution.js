812a7c1bbbfe8058fc0a15b609228cfdc0259a36
nowakoff <anowak@icstars.org>
2015-11-16 07:47:18 +0100

    game

--- week-7/design-basic-game-solo-challenge/my_solution.js
+++ week-7/design-basic-game-solo-challenge/my_solution.js
@@ -2,12 +2,12 @@
 
 // This is a solo challenge
 
-// Your mission description:
-// Overall mission:
-// Goals:
-// Characters:
-// Objects:
-// Functions:
+// Your mission description: tip tap toe
+// Overall mission: to put 3 elements in one column, acroos or in oneline
+// Goals: -||-
+// Characters: Kate, Tom
+// Objects: circle, x
+// Functions: insert into table 
 
 // Pseudocode
 //
@@ -18,21 +18,125 @@
 
 // Initial Code
 
+// var prisonerDilema = {
+
+// var prisoner = {
+// 	balance: 0, 
+// 	dilema:	function(card) {
+// 		var cards = ["cooperate", "defeat"]
+// 		var random = Math.floor((Math.random() * 2));
+// 		if (card === "cooperate" && cards[random] === "cooperate") {
+// 		prisoner.balance += 300; 
+// 		}
+// 		else if (card === "cooperate" && cards[random] === "defeat") {
+// 		prisoner.balance -= 100 ; 
+// 		}
+// 		else if (card === "defeat" && cards[random] === "defeat") {
+// 		prisoner.balance -= 10 ; 
+// 		}
+// 		else if (card === "defeat" && cards[random] === "cooperate") {
+// 		prisoner.balance += 500 ; 
+// 		}
+// 	console.log(prisoner.balance)
+//     },
+// }
+
+// while (prisoner.balance <= 1000 && prisoner.balance >= -1000 ) {
+// 	var cardType = prompt("Type one card name", "cooperate or defeat")
+// 	prisoner.dilema(cardType)
+// }
+
+// if (prisoner.balance >= 1000) {
+// 	var alertHappy = "Gratulation! You are a winner! You won ";
+// 	console.log(alertHappy.concat(prisoner.balance));
+
+// }
+// else {
+// 	var alertSad = "Sorry but game is over nad you lost ";
+// 	console.log(alertHappy.concat(prisoner.balance));
+// }
+
+// },
+	
+// inmate: {
+// 	balance: 0,
+// 	card: null
+// },
 
 
 
 
+// 	function dilema(cardP, cardI) {
+// if (cardP === "cooperate" && cardI === "scooperate") {
+// 	prisoner.balance += 300; 
+// 	inmate.balance += 300};
+// 	console.log(prisoner.balance,inmate.balance)
+// }
+
+
+// function dilema(cardP, cardI) {
+// if (cardP === "cooperate" && cardI === "scooperate") {
+// 	prisoner.balance += 300; 
+// 	inmate.balance += 300};
+// 	console.log(prisoner.balance,inmate.balance)
+// }
+
+
+
+
+// prisoner.card = prompt("Pick one card", "cooperate");
+//inmate.card = prompt("Pick one card", "cooperate");
+
+// console.log(prisoner.balance,inmate.balance)
 
 // Refactored Code
+var prisoner = {
+	balance: 0, 
+	dilema:	function(card) {
+		var cards = ["cooperate", "defeat"]
+		var random = Math.floor((Math.random() * 2));
+		if (card === "cooperate" && cards[random] === "cooperate") {
+		prisoner.balance += 300; 
+		}
+		else if (card === "cooperate" && cards[random] === "defeat") {
+		prisoner.balance -= 100 ; 
+		}
+		else if (card === "defeat" && cards[random] === "defeat") {
+		prisoner.balance -= 10 ; 
+		}
+		else if (card === "defeat" && cards[random] === "cooperate") {
+		prisoner.balance += 500 ; 
+		}
+	console.log(prisoner.balance)
+    },
+}
 
+while (prisoner.balance <= 1000 && prisoner.balance >= -1000 ) {
+	var cardType = prompt("Type one card name", "cooperate or defeat")
+	prisoner.dilema(cardType)
+}
+
+if (prisoner.balance >= 1000) {
+	var alertHappy = "Gratulation! You are a winner! You won ";
+	console.log(alertHappy.concat(prisoner.balance));
+
+}
+else {
+	var alertSad = "Sorry but game is over nad you lost ";
+	console.log(alertHappy.concat(prisoner.balance));
+}
 
 
 
 
 
 // Reflection
-//
-//
+// //What are the biggest similarities and differences between JavaScript and Ruby?
+// Syntax is more explicit in JS and you have to use much more code to execute some functionalities.
+// //Was some of your Ruby knowledge solidified by learning another language? If so, which concepts?
+// The concept of break and gets.chomp.
+// //How do you feel about diving into JavaScript after reading these chapters?
+// //I feel still a bit unsure of my knowledge.
 //
 //
 //
