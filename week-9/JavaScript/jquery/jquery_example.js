eef8916c104972da8ff183e32f41fa6cdf51ca9a
nowakoff <anowak@icstars.org>
2015-11-28 09:20:45 -0600

    ff

--- week-9/JavaScript/jquery/jquery_example.js
+++ week-9/JavaScript/jquery/jquery_example.js
@@ -17,20 +17,45 @@ $('body').css({'background-color': 'pink'})
 
 //RELEASE 2:
   //Add code here to select elements of the DOM
-
+var bodyElement = $('body')
 
 //RELEASE 3:
   // Add code here to modify the css and html of DOM elements
+$('h1').css({'color': 'blue'})
+$('h1').css({'border': '1px solid black'})
+$('h1').css({'visibility': 'visibile'})
 
 
 //RELEASE 4: Event Listener
   // Add the code for the event listener here
 
+$( ".mascot" ).hover(
+  function() {
+  $( this ).append( $( '<img src= "http://a.scpr.org/i/3711f59ecf5e01beefa4725a8459621f/32683-eight.jpg">' ) );
+  }, function() {
+  $( this ).find( "img:last" ).remove();
+  }
+);
+
+$( ".mascot.fade" ).hover(function() {
+  $( this ).fadeOut( 100 );
+  $( this ).fadeIn( 500 );
+});
+
+
+
+
 
 //RELEASE 5: Experiment on your own
 
+ 
 
 
+$( "#right" ).click(function() {
+  $( ".block" ).animate({ "left": "+=50px" }, "slow" );
+});
+
+ 
 
 
 
